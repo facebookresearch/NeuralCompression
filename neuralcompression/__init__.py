1ad0cde24fae909c7a73c668ffc48a5fc0c395a0
Matthew Muckley <matt.muckley@gmail.com>
2021-07-20 05:47:52 -0700

    Update version

--- neuralcompression/__init__.py
+++ neuralcompression/__init__.py
@@ -5,7 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-__version__ = "0.1.0.dev20210719"
+__version__ = "0.1.1a20210720"
 __author__ = "Facebook AI Research"
 __license__ = "MIT"
 __homepage__ = ""
