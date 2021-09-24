83f8c7acabb7f51a84cb019886bd4a9c06866a44
Allen Goodman <allen.goodman@icloud.com>
2021-09-23 20:10:07 -0400

    _HiFiCDiscriminator cleanup

--- neuralcompression/models/_hific/_hific_discriminator.py
+++ neuralcompression/models/_hific/_hific_discriminator.py
@@ -9,18 +9,21 @@ class _HiFiCDiscriminator(torch.nn.Module):
     def __init__(
             self,
             image_dimensions: typing.Tuple[int] = (3, 256, 256),
-            latent_dimensions: typing.Tuple[int] = (220, 16, 16),
-            bottleneck_depth: int = 220,
+            feature_dimensions: typing.Tuple[int] = (220, 16, 16),
+            features: int = 220,
             spectral_norm=True
     ):
         super(_HiFiCDiscriminator, self).__init__()
 
         self.image_dimensions = image_dimensions
-        self.latent_dimensions = latent_dimensions
+
+        self.feature_dimensions = feature_dimensions
+
+        self.features = features
 
         self.activation = torch.nn.LeakyReLU(0.2)
 
-        self.extract_features = torch.nn.Conv2d(bottleneck_depth, 12, 3, padding=1, padding_mode="reflect")
+        self.extract_features = torch.nn.Conv2d(self.features, 12, 3, padding=1, padding_mode="reflect")
 
         self.upsample = torch.nn.Upsample(None, 16, "nearest")
 
@@ -54,6 +57,6 @@ class _HiFiCDiscriminator(torch.nn.Module):
         x = self.activation(self.conv3(x))
         x = self.activation(self.conv4(x))
 
-        predictions = self.discriminate(x).view(-1, 1)
+        features = self.discriminate(x).view(-1, 1)
 
-        return torch.sigmoid(predictions), predictions
+        return torch.sigmoid(features), features
