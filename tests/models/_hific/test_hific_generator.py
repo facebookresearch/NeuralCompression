4d69f13c5db0bc921bbdfc9a872c301b0dda8acb
Allen Goodman <allen.goodman@icloud.com>
2021-09-26 19:08:46 -0400

    Update test_hific_generator.py

--- tests/models/_hific/test_hific_generator.py
+++ tests/models/_hific/test_hific_generator.py
@@ -1,3 +1,12 @@
+import torch
+
+from neuralcompression.models._hific._hific_generator import _HiFiCGenerator
+
+
 class TestHiFiCGenerator:
     def test_forward(self):
-        assert True
+        generator = _HiFiCGenerator((16, 16, 16), 8)
+
+        x = torch.rand((8, 16, 16, 16))
+
+        assert generator(x).shape == torch.Size([8, 3, 256, 256])
