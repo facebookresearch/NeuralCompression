eb1daa5253b9d4e454a416c33970a58268752ec1
Allen Goodman <allen.goodman@icloud.com>
2021-09-27 10:19:51 -0400

    Update test_hific_discriminator.py

--- tests/models/_hific/test_hific_discriminator.py
+++ tests/models/_hific/test_hific_discriminator.py
@@ -1,3 +1,16 @@
+import torch
+
+from neuralcompression.models._hific._hific_discriminator import _HiFiCDiscriminator
+
+
 class TestHiFiCDiscriminator:
     def test_forward(self):
-        assert True
+        discriminator = _HiFiCDiscriminator()
+
+        x = torch.rand((8, 3, 256, 256))
+        y = torch.rand((8, 220, 16, 16))
+
+        a, b = discriminator(x, y)
+
+        assert a.shape == torch.Size([2048, 1])
+        assert b.shape == torch.Size([2048, 1])
