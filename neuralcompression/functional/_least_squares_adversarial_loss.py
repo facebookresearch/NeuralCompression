d909cb7b2a220b7c864fa48cf3fe47ca592dbf76
Allen Goodman <allen.goodman@icloud.com>
2021-09-29 13:01:25 -0400

    least squares adversarial loss

---
+++ neuralcompression/functional/_least_squares_adversarial_loss.py
@@ -0,0 +1,17 @@
+import typing
+
+import torch
+
+
+def _least_squares_adversarial_loss(
+        authentic: torch.Tensor,
+        synthetic: torch.Tensor,
+) -> typing.Optional[typing.Tuple[torch.Tensor, torch.Tensor]]:
+    authentic_discriminator_loss = torch.mean(torch.square(authentic - 1.0))
+    synthetic_discriminator_loss = torch.mean(torch.square(synthetic))
+
+    discriminator_loss = 0.5 * (authentic_discriminator_loss + synthetic_discriminator_loss)
+
+    generator_loss = 0.5 * torch.mean(torch.square(synthetic - 1.0))
+
+    return discriminator_loss, generator_loss
