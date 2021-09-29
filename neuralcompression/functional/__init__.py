6577e65dd0d268a50c16b903bacf3ed280cbe445
Allen Goodman <allen.goodman@icloud.com>
2021-09-29 12:56:40 -0400

    non-saturating adversarial loss

--- neuralcompression/functional/__init__.py
+++ neuralcompression/functional/__init__.py
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+from ._non_saturating_adversarial_loss import non_saturating_adversarial_loss
 from .complexity import count_flops
 from .distortion import (
     learned_perceptual_image_patch_similarity,
---
+++ neuralcompression/functional/_non_saturating_adversarial_loss.py
@@ -0,0 +1,27 @@
+import typing
+
+import torch.nn.functional
+
+
+def non_saturating_adversarial_loss(
+        authentic: torch.Tensor,
+        synthetic: torch.Tensor,
+) -> typing.Optional[typing.Tuple[torch.Tensor, torch.Tensor]]:
+    authentic_discriminator_loss = torch.nn.functional.binary_cross_entropy_with_logits(
+        authentic,
+        torch.ones_like(authentic)
+    )
+
+    synthetic_discriminator_loss = torch.nn.functional.binary_cross_entropy_with_logits(
+        synthetic,
+        torch.zeros_like(synthetic)
+    )
+
+    discriminator_loss = authentic_discriminator_loss + synthetic_discriminator_loss
+
+    generator_loss = torch.nn.functional.binary_cross_entropy_with_logits(
+        synthetic,
+        torch.ones_like(synthetic)
+    )
+
+    return discriminator_loss, generator_loss
