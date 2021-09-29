1980b339d9b6efd64c5cd989340a9088812728bc
Allen Goodman <allen.goodman@icloud.com>
2021-09-29 16:27:17 -0400

    improved unit tests

--- tests/functional/test_soft_round_inverse.py
+++ tests/functional/test_soft_round_inverse.py
@@ -1,11 +1,29 @@
 import torch
 
-from neuralcompression.functional import soft_round_inverse
+from neuralcompression.functional import soft_round, soft_round_inverse
 
 
 def test_soft_round_inverse():
     x = torch.linspace(-2.0, 2.0, 50)
 
-    y = soft_round_inverse(x, alpha=1e-13)
+    torch.testing.assert_close(
+        x,
+        soft_round_inverse(x, alpha=1e-13),
+    )
 
-    torch.testing.assert_close(x, y)
+    x = torch.tensor([-1.25, -0.75, 0.75, 1.25])
+
+    torch.testing.assert_close(
+        x,
+        soft_round_inverse(soft_round(x, alpha=2.0), alpha=2.0),
+    )
+
+    for offset in range(-5, 5):
+        x = torch.linspace(offset + 0.001, offset + 0.999, 100)
+
+        torch.testing.assert_close(
+            torch.ceil(x) - 0.5,
+            soft_round_inverse(x, alpha=5000.0),
+            atol=0.001,
+            rtol=0.002,
+        )
