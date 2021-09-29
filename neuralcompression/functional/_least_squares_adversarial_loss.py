f2316de3bc484d3eb82f3d0ea3de8257ee6c176f
Allen Goodman <allen.goodman@icloud.com>
2021-09-29 13:53:22 -0400

    formatting

--- neuralcompression/functional/_least_squares_adversarial_loss.py
+++ neuralcompression/functional/_least_squares_adversarial_loss.py
@@ -4,13 +4,15 @@ import torch
 
 
 def least_squares_adversarial_loss(
-        authentic: torch.Tensor,
-        synthetic: torch.Tensor,
+    authentic: torch.Tensor,
+    synthetic: torch.Tensor,
 ) -> typing.Optional[typing.Tuple[torch.Tensor, torch.Tensor]]:
     authentic_discriminator_loss = torch.mean(torch.square(authentic - 1.0))
     synthetic_discriminator_loss = torch.mean(torch.square(synthetic))
 
-    discriminator_loss = 0.5 * (authentic_discriminator_loss + synthetic_discriminator_loss)
+    discriminator_loss = 0.5 * (
+        authentic_discriminator_loss + synthetic_discriminator_loss
+    )
 
     generator_loss = 0.5 * torch.mean(torch.square(synthetic - 1.0))
 
--- neuralcompression/functional/_non_saturating_adversarial_loss.py
+++ neuralcompression/functional/_non_saturating_adversarial_loss.py
@@ -4,24 +4,21 @@ import torch.nn.functional
 
 
 def non_saturating_adversarial_loss(
-        authentic: torch.Tensor,
-        synthetic: torch.Tensor,
+    authentic: torch.Tensor,
+    synthetic: torch.Tensor,
 ) -> typing.Optional[typing.Tuple[torch.Tensor, torch.Tensor]]:
     authentic_discriminator_loss = torch.nn.functional.binary_cross_entropy_with_logits(
-        authentic,
-        torch.ones_like(authentic)
+        authentic, torch.ones_like(authentic)
     )
 
     synthetic_discriminator_loss = torch.nn.functional.binary_cross_entropy_with_logits(
-        synthetic,
-        torch.zeros_like(synthetic)
+        synthetic, torch.zeros_like(synthetic)
     )
 
     discriminator_loss = authentic_discriminator_loss + synthetic_discriminator_loss
 
     generator_loss = torch.nn.functional.binary_cross_entropy_with_logits(
-        synthetic,
-        torch.ones_like(synthetic)
+        synthetic, torch.ones_like(synthetic)
     )
 
     return discriminator_loss, generator_loss
