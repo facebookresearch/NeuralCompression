acc1f5f69370adcfbd5440c92e889c51fc880d1a
Allen Goodman <allen.goodman@icloud.com>
2021-09-29 13:52:47 -0400

    formatting

--- neuralcompression/functional/_soft_round/_soft_round_inverse.py
+++ neuralcompression/functional/_soft_round/_soft_round_inverse.py
@@ -1,15 +1,19 @@
 import torch
 
 
-def soft_round_inverse(y: torch.Tensor, alpha: float, eps: float = 1e-3) -> torch.Tensor:
+def soft_round_inverse(
+    y: torch.Tensor, alpha: float, eps: float = 1e-3
+) -> torch.Tensor:
     alpha = torch.tensor(alpha)
 
     eps = torch.tensor(eps)
 
     maximum = torch.maximum(alpha, eps)
 
-    m = torch.floor(y) + .5
+    m = torch.floor(y) + 0.5
 
-    x = m + torch.clamp(torch.atanh((y - m) * (torch.tanh(maximum / 2.0) * 2.0)) / maximum, -0.5, 0.5)
+    x = m + torch.clamp(
+        torch.atanh((y - m) * (torch.tanh(maximum / 2.0) * 2.0)) / maximum, -0.5, 0.5
+    )
 
     return torch.where(alpha < eps, y, x)
