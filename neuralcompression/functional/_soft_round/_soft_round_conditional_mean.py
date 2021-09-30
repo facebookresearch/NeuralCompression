fe11b4841aa3cfd316d2e4619b500b722954376e
Allen Goodman <allen.goodman@icloud.com>
2021-09-30 11:47:05 -0400

    soft_round_conditional_mean docstring

--- neuralcompression/functional/_soft_round/_soft_round_conditional_mean.py
+++ neuralcompression/functional/_soft_round/_soft_round_conditional_mean.py
@@ -4,4 +4,29 @@ from ._soft_round_inverse import soft_round_inverse
 
 
 def soft_round_conditional_mean(x: torch.Tensor, alpha: float) -> torch.Tensor:
+    """Conditional mean of ``x`` given noisy soft rounded values.
+
+    Computes:
+
+    .. math::
+
+    g(z) = E[Y | s(Y) + U = z]
+
+        where :math:`s` is the soft-rounding function, :math:`U` is uniform
+        between :math:`-0.5` and :math:`0.5` and :math:`Y` is considered
+        uniform when truncated to the interval :math:`[z-0.5, z+0.5]`.
+
+    This operation is described in Sec. 4.1. in the paper:
+
+    > "Universally Quantized Neural Compression"<br />
+    > Eirikur Agustsson & Lucas Theis<br />
+    > https://arxiv.org/abs/2006.09952
+
+    Args:
+        x:
+        alpha: smoothness of the ``soft_round`` approximation
+
+    Returns:
+      The conditional mean, of same shape as `inputs`.
+    """
     return soft_round_inverse(x - 0.5, alpha) + 0.5
