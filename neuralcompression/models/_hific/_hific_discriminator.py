7da3b7df9d12d21233d8a17d8377cd75a70ae355
Allen Goodman <allen.goodman@icloud.com>
2021-09-30 17:59:42 -0400

    Update _hific_discriminator.py

--- neuralcompression/models/_hific/_hific_discriminator.py
+++ neuralcompression/models/_hific/_hific_discriminator.py
@@ -20,38 +20,41 @@ class HiFiCDiscriminator(torch.nn.Module):
         self.input_sequence = torch.nn.Sequential(
             torch.nn.LeakyReLU(0.2),
             torch.nn.Conv2d(
-                self.features, 12, (3, 3), padding=1, padding_mode="reflect"
+                self.features,
+                12,
+                (3, 3),
+                padding=1,
+                padding_mode="reflect",
             ),
             torch.nn.Upsample(None, 16, "nearest"),
         )
 
-        filters = (64, 128, 256, 512)
+        convolutions = []
 
-        conv_sequence = []
-        old_size = self.image_dimensions[0] + 12
-        for current_filter in filters:
-            conv_sequence.append(
-                torch.nn.utils.spectral_norm(
-                    torch.nn.Conv2d(
-                        old_size,
-                        current_filter,
-                        (4, 4),
-                        padding=1,
-                        padding_mode="reflect",
-                        stride=2,
-                    )
-                )
+        in_channels = self.image_dimensions[0] + 12
+
+        for out_channels in (64, 128, 256, 512):
+            convolution = torch.nn.Conv2d(
+                in_channels,
+                out_channels,
+                (4, 4),
+                (2, 2),
+                padding=1,
+                padding_mode="reflect",
             )
-            old_size = current_filter
 
-        conv_sequence.append(torch.nn.Conv2d(filters[3], 1, (1, 1), (1, 1)))
+            convolutions += [torch.nn.utils.spectral_norm(convolution)]
 
-        self.conv_sequence = torch.nn.Sequential(*conv_sequence)
+            in_channels = out_channels
+
+        convolutions += [torch.nn.Conv2d(512, 1, (1, 1), (1, 1))]
+
+        self.convolutions = torch.nn.Sequential(*convolutions)
 
     def forward(
         self, x: torch.Tensor, y: torch.Tensor
     ) -> typing.Tuple[torch.Tensor, torch.Tensor]:
-        predictions = self.conv_sequence(
+        predictions = self.convolutions(
             torch.cat((x, self.input_sequence(y)), 1)
         ).view(-1, 1)
 
