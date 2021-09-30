0c73af4ca8e7df652437f4ec9bfbf3dfd0138c56
Allen Goodman <allen.goodman@icloud.com>
2021-09-30 12:00:30 -0400

    docstring

--- neuralcompression/functional/_estimate_tails.py
+++ neuralcompression/functional/_estimate_tails.py
@@ -4,12 +4,40 @@ import torch
 
 
 def estimate_tails(
-    cdf: typing.Callable[[torch.Tensor], torch.Tensor],
+    func: typing.Callable[[torch.Tensor], torch.Tensor],
     target: float,
     shape: int,
     device: typing.Optional[typing.Union[int, str, torch.device]] = None,
     dtype: torch.dtype = torch.float32,
 ) -> torch.Tensor:
+    """Estimates approximate tail quantiles.
+
+    This runs a simple Adam iteration to determine tail quantiles. The
+        objective is to find an ``x`` such that:
+
+    ```
+    func(x) == target
+    ```
+
+    For instance, if ``func`` is a CDF and the target is a quantile value, this
+        would find the approximate location of that quantile. Note that
+        ``func`` is assumed to be monotonic. When each tail estimate has passed
+        the optimal value of ``x``, the algorithm does 100 additional
+        iterations and then stops. This operation is vectorized. The tensor
+        shape of ``x`` is given by `shape`, and `target` must have a shape that
+        is broadcastable to the output of ``func(x)``.
+
+    Args:
+        func: a function that computes cumulative distribution function,
+            survival function, or similar
+        target: desired target value
+        shape: shape representing ``x``
+        device: PyTorch device
+        dtype: PyTorch ``dtype`` of the computation and the return value
+
+    Returns:
+        the solution, ``x``
+    """
     if not device:
         if torch.cuda.is_available():
             device = "cuda"
@@ -27,7 +55,7 @@ def estimate_tails(
     variance = torch.ones(shape, dtype=dtype)
 
     while torch.min(counts) < 100:
-        abs(cdf(tails) - target).backward(torch.ones_like(tails))
+        abs(func(tails) - target).backward(torch.ones_like(tails))
 
         gradient = tails.grad.cpu()
 
