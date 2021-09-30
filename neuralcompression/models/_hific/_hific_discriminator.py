24e9dc3f5011022c288eaf8d5f5c1c53ea6ac6d9
Allen Goodman <allen.goodman@icloud.com>
2021-09-30 17:45:47 -0400

    Update neuralcompression/models/_hific/_hific_discriminator.py
    Co-authored-by: Matthew Muckley <mmuckley@fb.com>

--- neuralcompression/models/_hific/_hific_discriminator.py
+++ neuralcompression/models/_hific/_hific_discriminator.py
@@ -17,54 +17,42 @@ class HiFiCDiscriminator(torch.nn.Module):
 
         self.features = latent_features
 
-        self.activation = torch.nn.LeakyReLU(0.2)
-
-        self.conv_0 = torch.nn.Conv2d(
-            self.features, 12, (3, 3), padding=1, padding_mode="reflect"
+        self.input_sequence = torch.nn.Sequential(
+            torch.nn.LeakyReLU(0.2),
+            torch.nn.Conv2d(
+                self.features, 12, (3, 3), padding=1, padding_mode="reflect"
+            ),
+            torch.nn.Upsample(None, 16, "nearest"),
         )
-        self.conv_0 = torch.nn.utils.spectral_norm(self.conv_0)
-
-        self.upsample = torch.nn.Upsample(None, 16, "nearest")
 
         filters = (64, 128, 256, 512)
 
-        kwargs = {
-            "padding": 1,
-            "padding_mode": "reflect",
-            "stride": 2,
-        }
+        conv_sequence = []
+        old_size = self.image_dimensions[0] + 12
+        for current_filter in filters:
+            conv_sequence.append(
+                torch.nn.utils.spectral_norm(
+                    torch.nn.Conv2d(
+                        old_size,
+                        current_filter,
+                        (4, 4),
+                        padding=1,
+                        padding_mode="reflect",
+                        stride=2,
+                    )
+                )
+            )
+            old_size = current_filter
 
-        self.conv_1 = torch.nn.Conv2d(
-            self.image_dimensions[0] + 12, filters[0], (4, 4), **kwargs
-        )
-        self.conv_1 = torch.nn.utils.spectral_norm(self.conv_1)
+        conv_sequence.append(torch.nn.Conv2d(filters[3], 1, (1, 1), (1, 1)))
 
-        self.conv_2 = torch.nn.Conv2d(filters[0], filters[1], (4, 4), **kwargs)
-        self.conv_2 = torch.nn.utils.spectral_norm(self.conv_2)
-
-        self.conv_3 = torch.nn.Conv2d(filters[1], filters[2], (4, 4), **kwargs)
-        self.conv_3 = torch.nn.utils.spectral_norm(self.conv_3)
-
-        self.conv_4 = torch.nn.Conv2d(filters[2], filters[3], (4, 4), **kwargs)
-        self.conv_4 = torch.nn.utils.spectral_norm(self.conv_4)
-
-        self.predictions = torch.nn.Conv2d(filters[3], 1, (1, 1), (1, 1))
+        self.conv_sequence = torch.nn.Sequential(*conv_sequence)
 
     def forward(
         self, x: torch.Tensor, y: torch.Tensor
     ) -> typing.Tuple[torch.Tensor, torch.Tensor]:
-        y = self.activation(self.conv_0(y))
-
-        y = self.upsample(y)
-
-        x = torch.cat((x, y), 1)
-
-        x = self.activation(self.conv_1(x))
-
-        x = self.activation(self.conv_2(x))
-        x = self.activation(self.conv_3(x))
-        x = self.activation(self.conv_4(x))
-
-        predictions = self.predictions(x).view(-1, 1)
+        predictions = self.conv_sequence(
+            torch.cat((x, self.input_sequence(y)), 1)
+        ).view(-1, 1)
 
         return torch.sigmoid(predictions), predictions
