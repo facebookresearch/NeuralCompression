36f3d9d0589f32ead23b268bbc473681e2e686dd
Allen Goodman <allen.goodman@icloud.com>
2021-09-30 17:32:18 -0400

    soft rounding (#20)
    * soft rounding
    
    * formatting
    
    * formatting
    
    * formatting
    
    * formatting
    
    * improved unit tests
    
    * fix mypy issues
    
    * soft_round docstring
    
    * soft_round_inverse
    
    * soft_round_conditional_mean docstring
    
    * Update _soft_round_conditional_mean.py
    
    * docstring fixes
    
    * Update neuralcompression/functional/_soft_round/_soft_round_conditional_mean.py
    
    Co-authored-by: Matthew Muckley <mmuckley@fb.com>
    
    * Update neuralcompression/functional/_soft_round/_soft_round.py
    
    Co-authored-by: Matthew Muckley <mmuckley@fb.com>
    
    Co-authored-by: Matthew Muckley <mmuckley@fb.com>

---
+++ tests/functional/test_soft_round.py
@@ -0,0 +1,22 @@
+import torch
+
+from neuralcompression.functional import soft_round
+
+
+def test_soft_round():
+    x = torch.linspace(-2.0, 2.0, 50)
+
+    torch.testing.assert_close(
+        x,
+        soft_round(x, alpha=1e-13),
+    )
+
+    for offset in range(-5, 5):
+        x = torch.linspace(offset - 0.499, offset + 0.499, 100)
+
+        torch.testing.assert_close(
+            torch.round(x),
+            soft_round(x, alpha=2000.0),
+            atol=0.02,
+            rtol=0.02,
+        )
