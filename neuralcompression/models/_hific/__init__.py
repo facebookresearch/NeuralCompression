c52f46981c2fbdea560565857c8d8edd2d54fede
Allen Goodman <allen.goodman@icloud.com>
2021-10-01 14:32:46 -0400

    copyright headers

--- neuralcompression/models/_hific/__init__.py
+++ neuralcompression/models/_hific/__init__.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 from ._hific_discriminator import HiFiCDiscriminator
 from ._hific_encoder import HiFiCEncoder
 from ._hific_generator import HiFiCGenerator
