42942ae1601ae2d7f751f409f1791fcde409d644
Allen Goodman <allen.goodman@icloud.com>
2021-10-04 11:51:26 -0400

    HiFiC modules (#21)
    HiFiC modules

--- neuralcompression/models/_hific/__init__.py
+++ neuralcompression/models/_hific/__init__.py
@@ -4,3 +4,7 @@ Copyright (c) Facebook, Inc. and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+
+from ._hific_discriminator import HiFiCDiscriminator
+from ._hific_encoder import HiFiCEncoder
+from ._hific_generator import HiFiCGenerator
