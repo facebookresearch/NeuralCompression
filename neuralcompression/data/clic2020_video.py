0a28674fd4b7f09fbd0ef289c4553cb52bb010ef
Allen Goodman <allen.goodman@icloud.com>
2021-10-07 14:01:29 -0400

    Update clic2020_video.py

--- neuralcompression/data/clic2020_video.py
+++ neuralcompression/data/clic2020_video.py
@@ -34,7 +34,6 @@ class CLIC2020Video(Dataset):
                 <root>
                     └── [A-Za-z]_[720|1080|2160]P-[0-9a-z]{4}
                         └── [A-Za-z]_[720|1080|2160]P-[0-9a-z]{4}[0-9]{5}_[yuv].png
-
         split: The dataset split to use. One of
             {``train``, ``val``, ``test``}.
             Defaults to ``train``.
