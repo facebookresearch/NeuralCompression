0bc9f6cb745fa6e0fd25c6d03948ade89d901313
Allen Goodman <allen.goodman@icloud.com>
2021-10-07 18:45:41 -0400

    Update clic2020_video.py

--- neuralcompression/data/clic2020_video.py
+++ neuralcompression/data/clic2020_video.py
@@ -8,18 +8,17 @@ LICENSE file in the root directory of this source tree.
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 from time import sleep
-from typing import Callable, List, Optional, Type, Union
+from typing import Callable, Dict, List, Optional, Tuple, Type, Union
 from urllib.request import urlopen, urlretrieve
 from zipfile import ZipFile
 
-from PIL.Image import Image
 from pytorchvideo.data.clip_sampling import ClipSampler
 from pytorchvideo.data.frame_video import FrameVideo
 from pytorchvideo.data.utils import MultiProcessSampler
 from torch import Tensor, clamp, linspace
 from torch.utils.data import IterableDataset, RandomSampler, Sampler
+from torch.utils.data.dataset import T_co
 from torchvision.datasets.utils import verify_str_arg
-from torchvision.transforms import ToTensor
 from tqdm import tqdm
 
 
@@ -41,13 +40,17 @@ class CLIC2020Video(IterableDataset):
         download: If true, downloads the dataset from the internet and puts it
             in the root directory. If the dataset is already downloaded, it is
             not downloaded again.
+        video_sampler:
         transform: A callable that transforms a clip, e.g,
             ``pytorchvideo.transforms.RandomResizedCrop``.
         multithreaded_io: If true, performs input/output operations across
             multiple threads.
+        frames_per_clip:
     """
-
-    url_root = "https://storage.googleapis.com/clic2021_public/txt_files"
+    _current_video: Optional[Tuple[FrameVideo, int]] = None
+    _current_video_clip: Optional[Dict[str, Optional[Tensor]]]
+    _destination_root = "https://storage.googleapis.com/clic2021_public/txt_files"
+    _next_clip_start_sec: float = 0.0
 
     def __init__(
         self,
@@ -55,45 +58,59 @@ class CLIC2020Video(IterableDataset):
         clip_sampler: ClipSampler,
         split: str = "train",
         download: bool = False,
-        transform: Optional[Callable[[Tensor], Tensor]] = None,
+        transform: Optional[Callable[[Dict], Dict]] = None,
         video_sampler: Type[Sampler] = RandomSampler,
         multithreaded_io: bool = False,
         frames_per_clip: Optional[int] = None,
     ):
-        self.root = Path(root)
+        self._root = Path(root)
 
         self._clip_sampler = clip_sampler
 
-        self.split = verify_str_arg(split, "split", ("train", "val", "test"))
+        self._split = verify_str_arg(split, "split", ("train", "val", "test"))
 
         self._transform = transform
 
         if download:
             self.download()
 
-        self._video_paths = [Path(path.name) for path in self.root.glob("*")]
+        self._video_paths = [Path(path.name) for path in self._root.glob("*")]
 
         self._video_sampler = video_sampler(self._video_paths)
 
-        self._video_sampler_iterator = None
+        self._video_sampler_iterator = iter(MultiProcessSampler(self._video_sampler))
 
         if frames_per_clip:
             self._frames_per_clip = frames_per_clip
 
             self._frame_filter = self._sample_frames
 
-        self._current_video = None
-        self._current_video_clip = None
         self._next_clip_start_sec = 0.0
 
-    def __getitem__(self, video_index: int) -> dict:
-        video_frame_paths = [
-            *self.root.joinpath(self._video_paths[video_index]).glob("*_y.png")
-        ]
+    def __getitem__(self, index: int) -> Dict:
+        raise NotImplementedError
 
-        video = FrameVideo.from_frame_paths(video_frame_paths)
+    def __iter__(self):
+        return self
 
-        self._current_video = video, video_index
+    def __next__(self) -> Dict:
+        if not self._video_sampler_iterator:
+            self._video_sampler_iterator = iter(
+                MultiProcessSampler(self._video_sampler)
+            )
+
+        if self._current_video:
+            video, index = self._current_video
+        else:
+            index = next(self._video_sampler_iterator)
+
+            video_frame_paths = [
+                *self._root.joinpath(self._video_paths[index]).glob("*_y.png")
+            ]
+
+            video = FrameVideo.from_frame_paths(video_frame_paths)
+
+            self._current_video = video, index
 
         clip_info = self._clip_sampler(self._next_clip_start_sec, video.duration, {})
 
@@ -116,37 +133,23 @@ class CLIC2020Video(IterableDataset):
             "clip_index": clip_info.clip_index,
             "label": None,
             "video": self._current_video_clip["video"],
-            "video_index": video_index,
+            "video_index": index,
             "video_label": None,
-            "video_name": str(self._video_paths[video_index]),
+            "video_name": str(self._video_paths[index]),
         }
 
+        if self._transform:
+            sample = self._transform(sample)
+
         return sample
-
-    def __iter__(self):
-        if not self._video_sampler_iterator:
-            self._video_sampler_iterator = iter(MultiProcessSampler(self._video_sampler))
-
-        return self._video_sampler_iterator
-
-    def __next__(self) -> dict:
-        if not self._video_sampler_iterator:
-            self._video_sampler_iterator = iter(MultiProcessSampler(self._video_sampler))
-
-        if self._current_video:
-            _, index = self._current_video
-        else:
-            index = next(self._video_sampler_iterator)
-
-        return self[index]
 
     def __len__(self) -> int:
         return len(self._video_paths)
 
     def download(self):
-        self.root.mkdir(exist_ok=True, parents=True)
+        self._root.mkdir(exist_ok=True, parents=True)
 
-        with urlopen(f"{self.url_root}/video_urls.txt") as file:
+        with urlopen(f"{self._destination_root}/video_urls.txt") as file:
             endpoints = file.read().decode("utf-8").splitlines()
 
         def f(endpoint: str):
@@ -155,7 +158,7 @@ class CLIC2020Video(IterableDataset):
             path, _ = urlretrieve(endpoint)
 
             with ZipFile(path, "r") as archive:
-                archive.extractall(self.root)
+                archive.extractall(self._root)
 
         with tqdm(total=len(endpoints)) as progress:
             with ThreadPoolExecutor() as executor:
