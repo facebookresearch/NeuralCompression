4c11d44149d6a62ed2c0052a16e870b35f4af1c5
Allen Goodman <allen.goodman@icloud.com>
2021-10-08 19:37:00 -0400

    pmf_to_quantized_cdf op

--- docs/source/functional.rst
+++ docs/source/functional.rst
@@ -4,9 +4,10 @@ neuralcompression.functional
 .. currentmodule:: neuralcompression.functional
 
 .. autofunction:: count_flops
+.. autofunction:: dense_image_warp
+.. autofunction:: hsv2rgb
+.. autofunction:: information_content
 .. autofunction:: learned_perceptual_image_patch_similarity,
 .. autofunction:: multiscale_structural_similarity
-.. autofunction:: information_content
-.. autofunction:: hsv2rgb
 .. autofunction:: optical_flow_to_color
-.. autofunction:: dense_image_warp
+.. autofunction:: pmf_to_quantized_cdf
---
+++ neuralcompression/functional/_pmf_to_quantized_cdf.py
@@ -0,0 +1,70 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from torch import Tensor, cumsum, int64, round, zeros
+
+
+def pmf_to_quantized_cdf(pmf: Tensor, precision: int) -> Tensor:
+    """Transforms a probability mass function (PMF) into a quantized cumulative
+        distribution function (CDF) for entropy coding.
+
+    Because this op uses floating-point operations the quantized output may not
+        be consistent across multiple platforms. For entropy encoders and
+        decoders to have the same quantized CDF on different platforms, the
+        quantized CDF should be transformed, saved, and used on the multiple
+        platforms.
+
+    After quantization, if the PMF does not sum to :math:`2^{precision}`, then
+        some values of PMF are increased or decreased until the sum is equal to
+        :math:`2^{precision}`.
+
+    Args:
+        pmf: a probability mass function (PMF).
+        The PMF is **not** normalized by this op. The user is responsible for
+            normalizing the PMF, if necessary.
+        precision: the number of bits for probability quantization, must be
+            less than or equal to 16.
+
+    Returns:
+        The quantized CDF.
+    """
+    cdf = zeros(pmf.shape[0] + 1)
+
+    cdf[1:] = cumsum(pmf, dim=0)
+
+    cdf = round(cdf * (1 << precision) / cdf[-1]).to(int64)
+
+    for j in range(cdf.size(0) - 1):
+        if cdf[j] == cdf[j + 1]:
+            peak = (1 << precision) + 1
+
+            best = -1
+
+            for k in range(cdf.size(0) - 1):
+                frequency = int(cdf[k + 1] - cdf[k])
+
+                if 1 < frequency < peak:
+                    peak = frequency
+
+                    best = k
+
+            assert best != -1
+
+            if best < j:
+                for k in range(best + 1, j + 1):
+                    cdf[k] -= 1
+            else:
+                assert best > j
+
+                for k in range(j + 1, best + 1):
+                    cdf[k] += 1
+
+    assert cdf[0] == 0
+
+    assert cdf[-1] == 1 << precision
+
+    return cdf
