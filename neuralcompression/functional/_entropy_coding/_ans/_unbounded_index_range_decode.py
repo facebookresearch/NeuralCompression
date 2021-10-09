649dbb13cc7709f254525a4226ed623250eb1b18
Allen Goodman <allen.goodman@icloud.com>
2021-10-09 19:55:51 -0400

    Update _unbounded_index_range_decode.py

--- neuralcompression/functional/_entropy_coding/_ans/_unbounded_index_range_decode.py
+++ neuralcompression/functional/_entropy_coding/_ans/_unbounded_index_range_decode.py
@@ -1,2 +1,27 @@
-def unbounded_index_range_decode():
+from torch import Tensor
+
+
+def unbounded_index_range_decode(
+        data: Tensor,
+        index: Tensor,
+        cdf: Tensor,
+        cdf_size: Tensor,
+        offset: Tensor,
+        precision: int,
+        overflow_width: int,
+):
+    """Range decodes encoded data using an indexed probability table.
+
+    Args:
+        data:
+        index:
+        cdf:
+        cdf_size:
+        offset:
+        precision:
+        overflow_width:
+
+    Returns:
+        The decoded data.
+    """
     return
