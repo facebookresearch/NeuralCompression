38a6cbedeb87d4da8e517cefa47b0d639d48a0c0
Allen Goodman <allen.goodman@icloud.com>
2021-10-10 19:18:04 -0400

    lower_bound op

---
+++ neuralcompression/functional/_lower_bound.py
@@ -0,0 +1,33 @@
+from torch import Tensor, clamp, logical_or
+from torch.autograd import Function
+
+
+class _LowerBound(Function):
+    @staticmethod
+    def forward(
+        ctx,
+        tensor: Tensor,
+        lower_bound: int,
+        gradient: str = "identity_if_towards",
+    ) -> Tensor:
+        if gradient in ("disconnected", "identity", "identity_if_towards"):
+            ctx.gradient = gradient
+        else:
+            raise ValueError
+
+        ctx.mask = tensor.ge(lower_bound)
+
+        return clamp(tensor, lower_bound)
+
+    @staticmethod
+    def backward(ctx, grad_output: Tensor):
+        if ctx.gradient == "identity_if_towards":
+            grad_output *= logical_or(ctx.mask, grad_output.lt(0.0))
+
+        if ctx.gradient == "disconnected":
+            grad_output *= ctx.mask
+
+        return grad_output.type(grad_output.dtype), None
+
+
+lower_bound = _LowerBound.apply
