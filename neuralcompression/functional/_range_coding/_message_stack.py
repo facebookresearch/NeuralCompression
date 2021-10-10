9a445bcfb4cb22a7549fefefc1a68004943ebff2
Allen Goodman <allen.goodman@icloud.com>
2021-10-10 11:29:38 -0400

    unit test scaffolding

---
+++ neuralcompression/functional/_range_coding/_message_stack.py
@@ -0,0 +1,123 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from typing import Callable, Optional, Tuple
+
+from torch import Tensor, any, cat, div, full, int64, ravel, remainder, sum, tensor
+
+_MessageStack = Tuple[Tensor, Optional["_MessageStack"]]
+
+
+def _empty_message_stack(shape: Tuple[int, ...]) -> _MessageStack:
+    return full(shape, 1 << 31), ()
+
+
+def _message_stack_to_message(message_stack: _MessageStack) -> Tensor:
+    message, message_stack = message_stack
+
+    message = ravel(message)
+
+    messages = [message >> 32, message]
+
+    while message_stack:
+        message, message_stack = message_stack
+
+        messages += [message]
+
+    return cat(messages)
+
+
+def _message_to_message_stack(message: Tensor) -> _MessageStack:
+    return message[0] << 32 | message[1], (message[2:], ())
+
+
+def _pop_from_message_stack(
+    message_stack: _MessageStack,
+    precision: int,
+) -> Tuple[Tensor, Callable[[Tensor, Tensor], _MessageStack]]:
+    previous_message, previous_message_stack = message_stack
+
+    previous_message = previous_message.type(int64)
+
+    interval_starting_indicies = previous_message & (1 << precision) - 1
+
+    def f(starting_indicies: Tensor, frequencies: Tensor) -> _MessageStack:
+        messages = (
+            frequencies * (previous_message >> precision)
+            + interval_starting_indicies
+            - starting_indicies
+        )
+
+        indicies = messages < (1 << 31)
+
+        n = sum(indicies)
+
+        if n > 0:
+            next_message, next_message_stack = _slice_message_stack(
+                previous_message_stack, int(n)
+            )
+
+            try:
+                messages[indicies] = tensor(messages[indicies] << 32) | next_message
+            except TypeError:
+                messages = tensor(messages << 32) | next_message
+        else:
+            next_message_stack = previous_message_stack
+
+        return messages, next_message_stack
+
+    return interval_starting_indicies, f
+
+
+def _push_to_message_stack(
+    message_stack: _MessageStack,
+    starting_indicies: Tensor,
+    frequencies: Tensor,
+    precision: int,
+) -> _MessageStack:
+    message, message_stack = message_stack
+
+    assert message.shape == starting_indicies.shape == frequencies.shape
+
+    indicies = message >= (((1 << 31) >> precision) << 32) * frequencies
+
+    if any(indicies) > 0:
+        message_stack = message[indicies], message_stack
+
+        message[indicies] >>= 32
+
+    quotients = div(message, frequencies, rounding_mode="floor")
+
+    remainders = remainder(message, frequencies)
+
+    return (quotients << precision) + remainders + starting_indicies, message_stack
+
+
+def _slice_message_stack(
+    message_stack: _MessageStack,
+    n: int = 0,
+) -> _MessageStack:
+    if n == 0:
+        return message_stack
+
+    messages = []
+
+    while n > 0:
+        message, message_stack = message_stack
+
+        if n >= len(message):
+            messages += [message]
+
+            n -= len(message)
+        else:
+            messages += [message[:n]]
+
+            message_stack = message[n:], message_stack
+
+            break
+
+    return cat(messages), message_stack
---
+++ tests/functional/_range_coding/test_message_stack.py
@@ -0,0 +1,30 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+
+def test_empty_message_stack():
+    assert True
+
+
+def test_message_stack_to_message():
+    assert True
+
+
+def test_message_to_message_stack():
+    assert True
+
+
+def test_pop_from_message_stack():
+    assert True
+
+
+def test_push_to_message_stack():
+    assert True
+
+
+def test_slice_message_stack():
+    assert True
