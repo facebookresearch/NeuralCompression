194e8814f50af1a6650cb481c57277174ff167bd
Allen Goodman <allen.goodman@icloud.com>
2021-10-10 16:51:04 -0400

    Update _message_stack.py

--- neuralcompression/functional/_range_coding/_message_stack.py
+++ neuralcompression/functional/_range_coding/_message_stack.py
@@ -5,7 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from typing import cast, Any, Callable, Optional, Tuple
+from typing import cast, Callable, Optional, Tuple
 
 from torch import (
     Tensor,
