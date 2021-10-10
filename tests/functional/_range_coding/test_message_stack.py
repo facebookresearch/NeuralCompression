607e8b0cf74db8b305864e79950c1817a5c47411
Allen Goodman <allen.goodman@icloud.com>
2021-10-10 11:46:45 -0400

    test_empty_message_stack

--- tests/functional/_range_coding/test_message_stack.py
+++ tests/functional/_range_coding/test_message_stack.py
@@ -5,9 +5,37 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+from torch import tensor
+from torch.testing import assert_close
+
+from neuralcompression.functional._range_coding._message_stack import (
+    _empty_message_stack,
+)
+
 
 def test_empty_message_stack():
-    assert True
+    maximum = 1 << 31
+
+    actual_message, actual_message_stack = _empty_message_stack((1, 2))
+
+    expected_message = tensor([[maximum, maximum]])
+
+    assert_close(actual_message, expected_message)
+
+    assert actual_message_stack == ()
+
+    actual_message, actual_message_stack = _empty_message_stack((2, 2))
+
+    expected_message = tensor(
+        [
+            [maximum, maximum],
+            [maximum, maximum],
+        ]
+    )
+
+    assert_close(actual_message, expected_message)
+
+    assert actual_message_stack == ()
 
 
 def test_message_stack_to_message():
