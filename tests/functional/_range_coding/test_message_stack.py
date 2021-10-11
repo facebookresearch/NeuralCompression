3d01e01feb59e63f6e929e5d3ffcc9d6bba6a82f
Allen Goodman <allen.goodman@icloud.com>
2021-10-11 11:10:21 -0400

    fix import style

--- tests/functional/_range_coding/test_message_stack.py
+++ tests/functional/_range_coding/test_message_stack.py
@@ -5,8 +5,8 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from torch import all, int32, int64, ones, randint, tensor
-from torch.testing import assert_close
+import torch
+import torch.testing
 
 from neuralcompression.functional._range_coding._message_stack import (
     _empty_message_stack,
@@ -23,22 +23,22 @@ def test_empty_message_stack():
 
     actual_message, actual_message_stack = _empty_message_stack((1, 2))
 
-    expected_message = tensor([[maximum, maximum]])
+    expected_message = torch.tensor([[maximum, maximum]])
 
-    assert_close(actual_message, expected_message)
+    torch.testing.assert_close(actual_message, expected_message)
 
     assert actual_message_stack == ()
 
     actual_message, actual_message_stack = _empty_message_stack((2, 2))
 
-    expected_message = tensor(
+    expected_message = torch.tensor(
         [
             [maximum, maximum],
             [maximum, maximum],
         ]
     )
 
-    assert_close(actual_message, expected_message)
+    torch.testing.assert_close(actual_message, expected_message)
 
     assert actual_message_stack == ()
 
@@ -46,33 +46,33 @@ def test_empty_message_stack():
 def test_message_stack_to_message():
     message_stack_a = _empty_message_stack((7, 3))
 
-    for bit in randint(1 << 12, size=(100,) + (7, 3)):
+    for bit in torch.randint(1 << 12, size=(100,) + (7, 3)):
         message_stack_a = _push_to_message_stack(
             message_stack_a,
             bit,
-            ones((7, 3), dtype=int32),
+            torch.ones((7, 3), dtype=torch.int32),
             12,
         )
 
     message_a = _message_stack_to_message(message_stack_a)
 
-    assert message_a.dtype is int64
+    assert message_a.dtype is torch.int64
 
     message_stack_b = _message_to_message_stack(message_a, (7, 3))
 
-    assert_close(message_stack_a[0], message_stack_b[0])
+    torch.testing.assert_close(message_stack_a[0], message_stack_b[0])
 
     message_b = _message_stack_to_message(message_stack_b)
 
-    assert_close(message_a, message_b)
+    torch.testing.assert_close(message_a, message_b)
 
 
 def test_message_to_message_stack():
     message_stack = _empty_message_stack((8, 7))
 
-    starting_indicies = randint(0, 256, size=(1000,) + (8, 7))
+    starting_indicies = torch.randint(0, 256, size=(1000,) + (8, 7))
 
-    frequencies = randint(1, 256, size=(1000,) + (8, 7)) % (256 - starting_indicies)
+    frequencies = torch.randint(1, 256, size=(1000,) + (8, 7)) % (256 - starting_indicies)
 
     frequencies[frequencies == 0] = 1
 
@@ -86,18 +86,18 @@ def test_message_to_message_stack():
 
     encoded = _message_stack_to_message(message_stack)
 
-    assert encoded.dtype == int64
+    assert encoded.dtype == torch.int64
 
     message_stack = _message_to_message_stack(encoded, (8, 7))
 
     for start, frequency in reversed(list(zip(starting_indicies, frequencies))):
         cf, pop = _pop_from_message_stack(message_stack, 8)
 
-        assert all(start <= cf) and all(cf < start + frequency)
+        assert torch.all(start <= cf) and torch.all(cf < start + frequency)
 
         message_stack = pop(start, frequency)
 
-    assert all(message_stack[0] == _empty_message_stack((8, 7))[0])
+    assert torch.all(message_stack[0] == _empty_message_stack((8, 7))[0])
 
 
 def test_partition_message_stack():
@@ -112,9 +112,9 @@ def test_partition_message_stack():
         n=2,
     )
 
-    expected_message = tensor([[maximum, maximum], [maximum, maximum]])
+    expected_message = torch.tensor([[maximum, maximum], [maximum, maximum]])
 
-    assert_close(actual_message, expected_message)
+    torch.testing.assert_close(actual_message, expected_message)
 
     assert actual_message_stack == ()
 
@@ -127,9 +127,9 @@ def test_pop_from_message_stack():
         precision=16,
     )
 
-    expected_interval_starting_indicies = tensor([[0, 0]])
+    expected_interval_starting_indicies = torch.tensor([[0, 0]])
 
-    assert_close(
+    torch.testing.assert_close(
         actual_interval_starting_indicies,
         expected_interval_starting_indicies,
     )
@@ -142,14 +142,14 @@ def test_push_to_message_stack():
 
     actual_message, actual_message_stack = _push_to_message_stack(
         message_stack,
-        tensor([[0, 0]]),
-        tensor([[1, 1]]),
+        torch.tensor([[0, 0]]),
+        torch.tensor([[1, 1]]),
         precision=16,
     )
 
-    expected_message = tensor([[quantized, quantized]])
+    expected_message = torch.tensor([[quantized, quantized]])
 
-    assert_close(
+    torch.testing.assert_close(
         actual_message,
         expected_message,
     )
