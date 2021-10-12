33d6a25c5a0f4508de975fc47446d90b1bf4fe89
Allen Goodman <allen.goodman@icloud.com>
2021-10-12 10:26:07 -0400

    Update __init__.py

--- neuralcompression/distributions/__init__.py
+++ neuralcompression/distributions/__init__.py
@@ -1,4 +1,6 @@
 from ._monotonic import Monotonic
+from ._noisy_logistic import NoisyLogistic
+from ._noisy_normal import NoisyNormal
 from ._round import Round
 from ._soft_round import SoftRound
 from ._uniform_noise import UniformNoise
