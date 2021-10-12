d329986a1686c6c9bc1a2e199e4b2ad5d8d2df43
Allen Goodman <allen.goodman@icloud.com>
2021-10-12 14:51:47 -0400

    Update _log_sf.py

--- neuralcompression/functional/_log_sf.py
+++ neuralcompression/functional/_log_sf.py
@@ -5,20 +5,23 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from torch import Tensor, log1p, no_grad
-from torch.distributions import Distribution
+import torch
+from torch import Tensor
+from torch.distributions import Distribution, Normal
+
+from neuralcompression.functional import log_ndtr
 
 
 def log_sf(
-    value: Tensor,
+    x: Tensor,
     distribution: Distribution,
-):
+) -> Tensor:
     """Log of the survival function of ``x`` for ``distribution``.
 
     The log of the survival function is defined as :math:`1 - CDF`.
 
     Args:
-        value:
+        x:
         distribution: a parameterizable probability distribution from
             ``torch.distributions``, e.g. ``torch.distributions.Categorical``
             or ``torch.distributions.Normal``.
@@ -26,5 +29,7 @@ def log_sf(
     Returns:
         Log of the survival function.
     """
-    with no_grad():
-        return log1p(distribution.cdf(value))
+    if isinstance(distribution, Normal):
+        return log_ndtr(-x)
+
+    return torch.log1p(distribution.cdf(x))
