6ad90f8054a1c0fa863f5a1ef65395a9a9d173f9
Allen Goodman <allen.goodman@icloud.com>
2021-10-12 15:12:50 -0400

    fixes #50

--- tests/test_clic2020.py
+++ tests/test_clic2020.py
@@ -5,13 +5,14 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from random import choice
-
+import numpy
 from PIL.Image import Image
 from pytest import fixture
 
 from neuralcompression.data import CLIC2020
 from utils import create_random_image
+
+rng = numpy.random.default_rng(2021)
 
 
 @fixture
@@ -20,7 +21,7 @@ def data(tmp_path):
 
     directory.mkdir(parents=True)
 
-    n = choice(range(16))
+    n = int(rng.integers(1, 16, (1,)))
 
     for index in range(n):
         path = directory.joinpath(f"{index}.png")
