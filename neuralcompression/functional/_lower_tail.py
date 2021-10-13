40817269a91a86871305da8027f16bcf969862a0
Allen Goodman <allen.goodman@icloud.com>
2021-10-12 21:40:57 -0400

    docs

--- neuralcompression/functional/_lower_tail.py
+++ neuralcompression/functional/_lower_tail.py
@@ -4,20 +4,37 @@ from torch import Tensor
 from torch.distributions import Distribution
 
 from ._estimate_tails import estimate_tails
-from ._logcdf import logcdf
+from ._log_cdf import log_cdf
 
 
 def lower_tail(distribution: Distribution, tail_mass: float) -> Tensor:
+    """Approximates lower tail quantile for range coding.
+
+    For range coding of random variables, the distribution tails need special
+        handling, because range coding can only handle alphabets with a finite
+        number of symbols. This method returns a cut-off location for the lower
+        tail, such that approximately `tail_mass` probability mass is contained
+        in the tails (together). The tails are then handled by using the
+        ‘overflow’ functionality of the range coder implementation (using a
+        Golomb-like universal code).
+
+    Args:
+        distribution:
+        tail_mass: desired probability mass for the tails.
+
+    Returns:
+        the approximate lower tail quantiles for each scalar distribution.
+    """
     try:
         _lower_tail = distribution.icdf(tail_mass / 2)
     except (AttributeError, NotImplementedError):
         try:
 
-            def _logcdf(x: Tensor) -> Tensor:
-                return logcdf(x, distribution)
+            def _log_cdf(x: Tensor) -> Tensor:
+                return log_cdf(x, distribution)
 
             _lower_tail = estimate_tails(
-                _logcdf,
+                _log_cdf,
                 math.log(tail_mass / 2),
                 distribution.batch_shape,
             )
