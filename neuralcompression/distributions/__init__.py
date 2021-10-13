7eb7645261e9b0f62942707818e81f10b54d8ca5
Allen Goodman <allen.goodman@icloud.com>
2021-10-13 13:21:37 -0400

    Update __init__.py

--- neuralcompression/distributions/__init__.py
+++ neuralcompression/distributions/__init__.py
@@ -5,7 +5,9 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+from ._factorized import Factorized
 from ._monotonic import Monotonic
+from ._noisy_factorized import NoisyFactorized
 from ._noisy_logistic import NoisyLogistic
 from ._noisy_normal import NoisyNormal
 from ._round import Round
