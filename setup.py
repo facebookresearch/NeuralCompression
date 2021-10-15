d634de4eb531bf80e734ce4813eadef6bfd7e766
Allen Goodman <allen.goodman@icloud.com>
2021-10-15 16:06:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,27 +5,12 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-import os
-import re
 from pathlib import Path
 
 import setuptools
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-# from https://github.com/facebookresearch/ClassyVision/blob/master/setup.py
-# get version string from module
-with open(
-    os.path.join(os.path.dirname(__file__), "neuralcompression/__init__.py"), "r"
-) as f:
-    readval = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
-    if readval is None:
-        raise RuntimeError("Version not found.")
-    version = readval.group(1)
-    print("-- Building version " + version)
-
-
 setuptools.setup(
-    # version=version,
     project_urls={
         "Source": "https://github.com/facebookresearch/NeuralCompression",
     },
