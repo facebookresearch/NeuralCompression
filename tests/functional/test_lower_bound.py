374a90e2818221402a2405797c9839768691457d
Allen Goodman <allen.goodman@icloud.com>
2021-10-18 18:03:15 -0400

    Update test_lower_bound.py

--- tests/functional/test_lower_bound.py
+++ tests/functional/test_lower_bound.py
@@ -12,7 +12,33 @@ def test_lower_bound():
 
     (bound,) = rng.random(1)
 
-    y = lower_bound(x, bound)
+    y = lower_bound(x, bound, gradient="disconnected")
+
+    torch.testing.assert_equal(y, torch.clamp_max(x, bound))
+
+    y.backward(x)
+
+    assert x.grad is not None
+
+    torch.testing.assert_equal(x.grad, (x >= bound) * x)
+
+    x = torch.tensor(rng.random((4,)), dtype=torch.float, requires_grad=True)
+
+    (bound,) = rng.random(1)
+
+    y = lower_bound(x, bound, gradient="identity")
+
+    torch.testing.assert_equal(y, torch.clamp_max(x, bound))
+
+    y.backward(x)
+
+    assert x.grad is not None
+
+    x = torch.tensor(rng.random((4,)), dtype=torch.float, requires_grad=True)
+
+    (bound,) = rng.random(1)
+
+    y = lower_bound(x, bound, gradient="identity_if_towards")
 
     torch.testing.assert_equal(y, torch.clamp_max(x, bound))
 
