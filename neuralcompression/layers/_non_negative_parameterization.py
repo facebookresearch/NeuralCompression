ef834aa86453e07c862508e0b697de69e98d0df7
Allen Goodman <allen.goodman@icloud.com>
2021-10-20 12:55:14 -0400

    Update _non_negative_parameterization.py

--- neuralcompression/layers/_non_negative_parameterization.py
+++ neuralcompression/layers/_non_negative_parameterization.py
@@ -16,6 +16,33 @@ import neuralcompression.functional as ncF
 
 
 class NonNegativeParameterization(torch.nn.Module):
+    """Non-negative parameter as required by generalized divisive normalization
+        (GDN) transformations.
+
+    The parameter is subjected to an invertible transformation that slows down
+    the learning rate for small values.
+
+    Args:
+        initial_value: the initial value of the kernel. If not provided, its
+            ``shape`` must be given, and the initial value of the parameter
+            will be undefined.
+        minimum: lower bound for the parameter (defaults to ``0.0``).
+        offset: offset added to the reparameterization. The parameterization of
+            beta or gamma as their square roots lets the training slow down
+            when values are close to zero, which is desirable as small values
+            in the denominator can lead to a situation where gradient noise on
+            beta or gamma leads to extreme amounts of noise in the GDN
+            activations. However, without an ``offset``, we would get zero
+            gradients if any elements of beta or gamma were exactly zero, and
+            thus the training could get stuck. To prevent this, we add this
+            small constant. The default value was empirically determined as a
+            good starting point. Making it bigger potentially leads to more
+            gradient noise on the activations, making it too small may lead to
+            numerical precision issues.
+        shape: shape of the initial value of the kernel, ignored unless
+            ``initial_value`` is ``None``.
+    """
+
     initial_value: Optional[Tensor]
     _pedestal: Tensor
 
