b13c42b3812a2c17d79e7b34df823cd1a2a2ce51
Allen Goodman <allen.goodman@icloud.com>
2021-10-21 12:59:52 -0400

    Update test_log_expm1.py

--- tests/functional/test_log_expm1.py
+++ tests/functional/test_log_expm1.py
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+import numpy.random
 import torch
 import torch.testing
 
@@ -12,6 +13,15 @@ from neuralcompression.functional import log_expm1
 
 
 def test_log_expm1():
-    assert torch.isfinite(log_expm1(torch.tensor(0.00001)))
+    rng = numpy.random.default_rng(0xFEEEFEEE)
 
-    assert torch.isfinite(log_expm1(torch.tensor(10000.0)))
+    x = rng.random((32,), dtype=numpy.float)
+
+    actual = log_expm1(torch.tensor(x, dtype=torch.float))
+
+    assert torch.isfinite(actual).all()
+
+    torch.testing.assert_close(
+        actual,
+        torch.tensor(numpy.log(numpy.expm1(x)), dtype=torch.float),
+    )
