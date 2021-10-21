6b964bf5fce99d9b41deef58993dde3417c76d3e
Allen Goodman <allen.goodman@icloud.com>
2021-10-21 13:43:24 -0400

    HyperSynthesisTransformation layer
    closes #68

--- neuralcompression/layers/__init__.py
+++ neuralcompression/layers/__init__.py
@@ -4,4 +4,6 @@ Copyright (c) Facebook, Inc. and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+
+from ._hyper_synthesis_transformation import HyperSynthesisTransformation
 from .gdn import SimplifiedGDN, SimplifiedInverseGDN
---
+++ neuralcompression/layers/_hyper_synthesis_transformation.py
@@ -0,0 +1,19 @@
+from torch import Tensor
+from torch.nn import Conv2d, ConvTranspose2d, Module, ReLU, Sequential
+
+
+class HyperSynthesisTransformation(Module):
+    def __init__(self, m: int, n: int):
+        super(HyperSynthesisTransformation, self).__init__()
+
+        self._modules = Sequential(
+            ConvTranspose2d(n, n, (5, 5), (2,), (2,), (1,)),
+            ReLU(inplace=True),
+            ConvTranspose2d(n, n, (5, 5), (2,), (2,), (1,)),
+            ReLU(inplace=True),
+            Conv2d(n, m, (3, 3), (1,), 1),
+            ReLU(inplace=True),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._modules(x)
