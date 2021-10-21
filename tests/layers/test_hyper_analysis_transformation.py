82e6deccb9404d380c8983b38284642881603e66
Allen Goodman <allen.goodman@icloud.com>
2021-10-21 14:22:53 -0400

    TestHyperAnalysisTransformation

---
+++ tests/layers/test_hyper_analysis_transformation.py
@@ -0,0 +1,13 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+
+class TestHyperAnalysisTransformation:
+    def test___init__(self):
+        assert True
+
+    def test_forward(self):
+        assert True
