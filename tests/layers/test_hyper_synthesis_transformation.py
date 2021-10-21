8a0172fb6614e58e4f7fa6f20665e82e347406f6
Allen Goodman <allen.goodman@icloud.com>
2021-10-21 13:52:58 -0400

    TestHyperSynthesisTransformation

---
+++ tests/layers/test_hyper_synthesis_transformation.py
@@ -0,0 +1,14 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+
+class TestHyperSynthesisTransformation:
+    def test___init__(self):
+        assert True
+
+    def test_forward(self):
+        assert True
