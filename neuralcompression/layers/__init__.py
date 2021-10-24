790394c196f5435c229e38e3534fcfdaa242565e
Allen Goodman <allen.goodman@icloud.com>
2021-10-24 19:41:18 -0400

    2d

--- neuralcompression/layers/__init__.py
+++ neuralcompression/layers/__init__.py
@@ -5,5 +5,5 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from ._hyper_analysis_transformation import HyperAnalysisTransformation2D
+from ._hyper_analysis_transformation_2d import HyperAnalysisTransformation2D
 from .gdn import SimplifiedGDN, SimplifiedInverseGDN
