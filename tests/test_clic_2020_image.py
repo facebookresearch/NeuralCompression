69d4aa4deae9c67418ba434fa5438dfac9d03089
Allen Goodman <allen.goodman@icloud.com>
2021-10-25 10:45:32 -0400

    Update test_clic_2020_image.py

--- tests/test_clic_2020_image.py
+++ tests/test_clic_2020_image.py
@@ -5,8 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-import random
-
+import numpy
 import pytest
 from PIL.Image import Image
 
@@ -16,11 +15,13 @@ from utils import create_random_image
 
 @pytest.fixture
 def data(tmp_path):
+    rng = numpy.random.default_rng(0xFEEEFEEE)
+
     directory = tmp_path.joinpath("clic2020").joinpath("test")
 
     directory.mkdir(parents=True)
 
-    n = random.choice(range(16))
+    n = int(rng.integers(1, 16, (1,)))
 
     for index in range(n):
         path = directory.joinpath(f"{index}.png")
