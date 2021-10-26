9d5e8520a57278de120a68cd8f868479caded269
Allen Goodman <allen.goodman@icloud.com>
2021-10-26 18:16:17 -0400

    Move setup.py metadata into setup.cfg
    closes #131

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ Copyright (c) Facebook, Inc. and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+
 import os
 import re
 from pathlib import Path
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 # from https://github.com/facebookresearch/ClassyVision/blob/master/setup.py
@@ -22,38 +23,8 @@ with open(
     version = readval.group(1)
     print("-- Building version " + version)
 
-with open("README.md", encoding="utf8") as f:
-    readme = f.read()
-
 setup(
-    name="neuralcompression",
-    version=version,
-    description="A collection of tools for neural compression enthusiasts.",
-    long_description_content_type="text/markdown",
-    long_description=readme,
-    author="Facebook AI Research",
-    license="MIT",
-    project_urls={
-        "Source": "https://github.com/facebookresearch/NeuralCompression",
-    },
-    python_requires=">=3.6",
-    setup_requires=["wheel"],
-    packages=find_packages(
-        exclude=[
-            "tests",
-            "projects",
-        ]
-    ),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: System :: Archiving :: Compression",
-    ],
+    cmdclass={"build_ext": BuildExtension},
     ext_modules=[
         CppExtension(
             "neuralcompression.ext._pmf_to_quantized_cdf",
@@ -67,5 +38,5 @@ setup(
             ],
         ),
     ],
-    cmdclass={"build_ext": BuildExtension},
+    version=version,
 )
