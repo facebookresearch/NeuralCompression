5fcc3c488a97934adb50e3c9574391af63dcf9f0
Allen Goodman <allen.goodman@icloud.com>
2021-11-04 11:00:34 -0400

    Update __init__.py

--- neuralcompression/data/__init__.py
+++ neuralcompression/data/__init__.py
@@ -7,5 +7,6 @@ LICENSE file in the root directory of this source tree.
 
 from ._clic_2020_image import CLIC2020Image
 from ._clic_2020_video import CLIC2020Video
+from ._food_101 import Food101
 from .kodak import Kodak
 from .vimeo90k import Vimeo90kSeptuplet
