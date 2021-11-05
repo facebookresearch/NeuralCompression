f1edd290eed0ffff2ac049c9c8d22ffb8580955f
Allen Goodman <allen.goodman@icloud.com>
2021-11-05 12:55:40 -0400

    Update test_continuous_batched_entropy.py

---
+++ tests/layers/test_continuous_batched_entropy.py
@@ -0,0 +1,29 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+
+class TestContinuousBatchedEntropy:
+    def test___init__(self):
+        assert True
+
+    def test__compute_indexes(self):
+        assert True
+
+    def test__pmf_to_cdf(self):
+        assert True
+
+    def test_compress(self):
+        assert True
+
+    def test_forward(self):
+        assert True
+
+    def test_non_integer_offsets(self):
+        assert True
+
+    def test_quantization_offset(self):
+        assert True
