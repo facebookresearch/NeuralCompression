8c30f3cb11e1b03c0427d4d0d2216f2bd54295a4
Allen Goodman <allen.goodman@icloud.com>
2021-11-16 10:57:18 -0500

    Update _rate_mse_distortion_loss.py
    Update _rate_mse_distortion_loss.py

--- neuralcompression/layers/__init__.py
+++ neuralcompression/layers/__init__.py
@@ -9,6 +9,6 @@ from ._generalized_divisive_normalization import GeneralizedDivisiveNormalizatio
 from ._hyper_analysis_transformation_2d import HyperAnalysisTransformation2D
 from ._hyper_synthesis_transformation_2d import HyperSynthesisTransformation2D
 from ._non_negative_parameterization import NonNegativeParameterization
-from ._rate_distortion_loss import RateDistortionLoss
+from ._rate_mse_distortion_loss import RateMSEDistortionLoss
 from ._synthesis_transformation_2d import SynthesisTransformation2D
 from .gdn import SimplifiedGDN, SimplifiedInverseGDN
---
+++ neuralcompression/layers/_rate_mse_distortion_loss.py
@@ -0,0 +1,78 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import math
+from typing import List, NamedTuple
+
+import torch
+from torch import Tensor
+from torch.nn import Module, MSELoss
+
+
+class RateDistortionLoss(NamedTuple):
+    distortion: float
+    rate: float
+    rate_distortion: float
+
+
+class RateMSEDistortionLoss(Module):
+    """Rate-distortion loss.
+
+    The rate-distortion loss is the minimum transmission bit-rate for a
+    required quality. It can be obtained without consideration of a specific
+    coding method. Rate is expressed in bits per pixel (BPP) of the original,
+    ``x``, distortion is expressed as the mean squared error (MSE) between the
+    original, ``x``, and the target, ``x_hat``.
+
+    Args:
+        trade_off: rate-distortion trade-off. `trade_off = 1` is the solution
+            where the `(rate, distortion)` pair minimizes `rate + distortion`.
+            Increasing `trade_off` will penalize the distortion term so more
+            bits are spent.
+        maximum: dynamic range of the input (i.e. the difference between the
+            maximum the and minimum permitted values).
+    """
+
+    def __init__(self, trade_off: float = 1e-2, maximum: int = 255):
+        super(RateMSEDistortionLoss, self).__init__()
+
+        self._maximum = maximum
+
+        self._trade_off = trade_off
+
+        self._mse = MSELoss()
+
+    def forward(
+        self,
+        x_hat: Tensor,
+        bits: List[Tensor],
+        x: Tensor,
+    ) -> RateDistortionLoss:
+        """
+        Args:
+            x_hat: encoder output.
+            bits: predicted bits.
+            x: encoder input.
+        """
+        n, _, h, w = x.size()
+
+        bpps = []
+
+        for score in bits:
+            pixels = -math.log(2) * (n * h * w)
+
+            bpps += [float(torch.log(score).sum() / pixels)]
+
+        rate = sum(bpps)
+
+        distortion = self._mse(x_hat, x)
+
+        rate_distortion = rate + distortion
+
+        return RateDistortionLoss(
+            rate,
+            distortion,
+            self._trade_off * self._maximum ** 2 * rate_distortion,
+        )
