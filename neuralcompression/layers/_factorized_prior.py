fca68b94d0478b5823af782ac61e64ba2a097898
Allen Goodman <allen.goodman@icloud.com>
2021-11-19 11:43:24 -0500

    Update _factorized_prior.py

--- neuralcompression/layers/_factorized_prior.py
+++ neuralcompression/layers/_factorized_prior.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, NamedTuple, OrderedDict, Tuple
+from typing import List, OrderedDict, Tuple
 
 import torch
 from compressai.entropy_models import EntropyBottleneck
