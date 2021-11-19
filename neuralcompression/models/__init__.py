0f42898ca49d8ae769850a9bb9e90b8d0310f6ab
Allen Goodman <allen.goodman@icloud.com>
2021-11-19 14:03:12 -0500

    Update __init__.py

--- neuralcompression/models/__init__.py
+++ neuralcompression/models/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from ._factorized_prior import FactorizedPrior
 from ._hific import HiFiCDiscriminator, HiFiCEncoder, HiFiCGenerator
 from ._prior import Prior
 from .deep_video_compression import DVC
