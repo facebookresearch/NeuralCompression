d1368562ede04c9d6b0bc55b43232605a19ef859
Allen Goodman <allen.goodman@icloud.com>
2021-12-13 17:44:52 -0500

    remove metadata

--- neuralcompression/__init__.py
+++ neuralcompression/__init__.py
@@ -3,10 +3,4 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-__version__ = "0.1.1a20210720"
-__author__ = "Facebook AI Research"
-__license__ = "MIT"
-__homepage__ = ""
-__docs__ = "A collection of tools for neural compression enthusiasts."
-
 from . import data, entropy_coders, functional, layers, metrics, models
