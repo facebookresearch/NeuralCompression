78146d28aaf0be10e18678c3a1f99b01d74bd2c6
Matthew Muckley <matt.muckley@gmail.com>
2022-01-05 13:54:37 -0800

    Unused import

--- tests/data/test_clic_2020_video.py
+++ tests/data/test_clic_2020_video.py
@@ -3,8 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import random
-
 import pytest
 import pytorchvideo.data.clip_sampling
 from utils import create_random_image
