d74bebddab1e2a3a3f39b3fbe863d8557b7ceb70
Matthew Muckley <matt.muckley@gmail.com>
2022-01-06 11:20:25 -0800

    Update to use separate loading function

--- neuralcompression/ext/__init__.py
+++ neuralcompression/ext/__init__.py
@@ -5,13 +5,15 @@
 
 from pathlib import Path
 
-from torch.utils.cpp_extension import load
+from ._extension_loader import load_extension
 
-print(str(Path(__file__).resolve().parent / "pmf_to_quantized_cdf_py.cc"))
-_pmf_to_quantized_cdf = load(
-    name="_pmf_to_quantized_cdf",
-    sources=[str(Path(__file__).resolve().parent / "pmf_to_quantized_cdf_py.cc")],
-    verbose=True,
+# load pmf_to_quantized_cdf
+_extension_folder = Path(__file__).resolve().parent
+_extension_name = "_pmf_to_quantized_cdf"
+_extension_files = ["pmf_to_quantized_cdf_py.cc"]
+
+_pmf_to_quantized_cdf = load_extension(
+    _extension_name, _extension_folder, _extension_files
 )
 
 from _pmf_to_quantized_cdf import pmf_to_quantized_cdf  # noqa: E402
