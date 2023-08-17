0d2d9c234360d4e93c70f009519100c822cab90c
Matthew Muckley <matt.muckley@gmail.com>
2023-08-16 23:54:22 -0400

    Add model zoo and MS-ILLM pretrained weights (#211)
    * Add model zoo
    
    * Add license
    
    * Fix weights check
    
    * Change default pretraining
    
    * Move dependencies def
    
    * Try raw text
    
    * Finish README for zoo
    
    * Add eval folder example
    
    * Remove branch from eval example

---
+++ neuralcompression/zoo/_noganms.py
@@ -0,0 +1,93 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Optional
+
+import torch
+
+from neuralcompression.models import HiFiCAutoencoder
+
+VALID_WEIGHTS = [
+    "target_0.035bpp",
+    "target_0.07bpp",
+    "target_0.14bpp",
+    "target_0.3bpp",
+    "target_0.45bpp",
+    "target_0.9bpp",
+]
+
+
+def _build_noganms(weights: Optional[str] = None):
+    model = HiFiCAutoencoder()
+    if weights is not None:
+        if weights not in VALID_WEIGHTS:
+            raise ValueError(
+                f"Unrecognized weights {weights}, must be one of {VALID_WEIGHTS}"
+            )
+
+        url = (
+            "https://dl.fbaipublicfiles.com/NeuralCompression/2023-muckley-msillm/"
+            + f"noganms_{weights}.ckpt"
+        )
+
+        model.load_state_dict(
+            torch.hub.load_state_dict_from_url(url, map_location="cpu")
+        )
+
+    return model
+
+
+def noganms_quality_1(pretrained=False, **kwargs):
+    if pretrained is True:
+        weights = "target_0.035bpp"
+    else:
+        weights = None
+
+    return _build_noganms(weights=weights)
+
+
+def noganms_quality_2(pretrained=False, **kwargs):
+    if pretrained is True:
+        weights = "target_0.07bpp"
+    else:
+        weights = None
+
+    return _build_noganms(weights=weights)
+
+
+def noganms_quality_3(pretrained=False, **kwargs):
+    if pretrained is True:
+        weights = "target_0.14bpp"
+    else:
+        weights = None
+
+    return _build_noganms(weights=weights)
+
+
+def noganms_quality_4(pretrained=False, **kwargs):
+    if pretrained is True:
+        weights = "target_0.3bpp"
+    else:
+        weights = None
+
+    return _build_noganms(weights=weights)
+
+
+def noganms_quality_5(pretrained=False, **kwargs):
+    if pretrained is True:
+        weights = "target_0.455bpp"
+    else:
+        weights = None
+
+    return _build_noganms(weights=weights)
+
+
+def noganms_quality_6(pretrained=False, **kwargs):
+    if pretrained is True:
+        weights = "target_0.9bpp"
+    else:
+        weights = None
+
+    return _build_noganms(weights=weights)
