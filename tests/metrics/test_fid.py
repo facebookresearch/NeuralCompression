240faa625a2dfdc63eb4fbdb612645f5a9c6bec3
Matthew Muckley <matt.muckley@gmail.com>
2023-08-18 10:08:27 -0700

    Deprecated actions, FID test fix

--- tests/metrics/test_fid.py
+++ tests/metrics/test_fid.py
@@ -11,18 +11,21 @@ import neuralcompression.functional as ncF
 from neuralcompression.metrics import FrechetInceptionDistance
 
 
-@pytest.mark.parametrize("num_samples", [5])
+@pytest.mark.parametrize("num_samples", [10])
 def test_dists(num_samples: int, arange_4d_image: Tensor):
     if arange_4d_image.shape[1] != 3:
         return
 
+    rng = torch.Generator()
+    rng.manual_seed(55)
+
     metric = FrechetInceptionDistance()
 
     for _ in range(num_samples):
-        sample1 = torch.randn_like(arange_4d_image).abs()
+        sample1 = torch.randn(size=arange_4d_image.shape, generator=rng).abs()
         sample1 = sample1 / sample1.max()
         sample1 = ncF.image_to_255_scale(sample1, torch.uint8)
-        sample2 = torch.randn_like(arange_4d_image).abs()
+        sample2 = torch.randn(size=arange_4d_image.shape, generator=rng).abs()
         sample2 = sample2 / sample2.max()
         sample2 = ncF.image_to_255_scale(sample2, torch.uint8)
 
